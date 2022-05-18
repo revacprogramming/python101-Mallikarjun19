c5c5b1ab119ccd3b530b42e532e8d728bb4c061b
Mallikarjun19 <96414039+Mallikarjun19@users.noreply.github.com>
2022-05-18 04:45:03 +0000

    10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -2,3 +2,35 @@
 
 filename = "dataset/mbox-short.txt"
 
+fname=input("Enter the file name: ")
+fh=open(fname)
+l=[]
+d={}
+l1=[]
+
+for line in fh:
+    if line.startswith("From"):
+        x=line.rstrip().split()
+        mail=x[1]
+        l.append(mail)
+
+size=len(l)
+for i in range(0,size):
+    count=l.count(l[i])/2
+    ele=l[i]
+    if ele in d:
+        continue
+    else:
+        d[ele]=count
+        l1.append(ele)
+    
+#dsize=len(d)
+l1size=len(l1)
+
+keys=list(d.keys())
+vals=list(d.values())
+
+maxkey=keys[vals.index(max(vals))]
+maxval=max(vals)
+
+print(maxkey,maxval)
